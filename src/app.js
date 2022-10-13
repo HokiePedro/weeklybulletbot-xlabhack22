6ec49b0259b635a2b62971e8d54427293670e7a4
Nathan Diddle <nathan.diddle@maxar.com>
2022-10-13 13:36:52 -0400

    Add timber and updated gitignore

--- src/app.js
+++ src/app.js
@@ -3,12 +3,25 @@ const { App } = require('@slack/bolt');
 // Initializes your app with your bot token and signing secret
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode:true,
+    appToken: process.env.APP_TOKEN,
+    port: process.env.PORT
 });
+
+app.command('/timber', async ({ command, ack, say }) => {
+    try {
+      await ack();
+      say("Yaaay! that command works!" + command.text);
+    } catch (error) {
+        console.log("err")
+      console.error(error);
+    }
+});
+
 
 (async () => {
     // Start your app
-    await app.start(process.env.PORT || 3000);
-
+    await app.start();
     console.log('⚡️ Bolt app is running!');
 })();
