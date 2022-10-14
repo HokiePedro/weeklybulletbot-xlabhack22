75ceaab9d4cff481fcdf6438536401b23b266b39
Nathan Diddle <nathan.diddle@gmail.com>
2022-10-14 12:17:02 -0400

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,11 @@
-export NODE_ENV=local
+export NODE_ENV=
 export SLACK_BOT_TOKEN=
 export SLACK_SIGNING_SECRET=
+export APP_TOKEN=
 export PORT=3000
+
+export DB_USERNAME=
+export DB_PASSWORD=
+export DB_HOST=
+export DB_DATABASE=
+export DB_PORT=
