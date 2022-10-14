ab99bb91c668cf6de9b12e533cb81736c0fa2dbc
Nathan Diddle <nathan.diddle@gmail.com>
2022-10-14 15:19:54 -0400

    remove confusion between token values

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
-export NODE_ENV=
-export SLACK_BOT_TOKEN=
+export NODE_ENV=local
+export SLACK_BOT_TOKEN=xoxb-
 export SLACK_SIGNING_SECRET=
-export APP_TOKEN=
+export APP_TOKEN=xapp-
 export PORT=3000
 
 export DB_USERNAME=
